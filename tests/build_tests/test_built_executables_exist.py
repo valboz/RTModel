1df72b925f9702307bbec57c291996592d12dc3b
golmschenk <greg@olmschenk.com>
2024-04-04 19:26:41 -0400

    PEP 8 cleanup

--- tests/build_tests/test_built_executables_exist.py
+++ tests/build_tests/test_built_executables_exist.py
@@ -8,20 +8,24 @@ def test_reader_executable_exists():
     reader_executable_path = Path(rt_model.bindir + rt_model.readerexe)
     assert reader_executable_path.exists()
 
+
 def test_init_cond_executable_exists():
     rt_model = RTModel()
     init_cond_executable_path = Path(rt_model.bindir + rt_model.initcondexe)
     assert init_cond_executable_path.exists()
+
 
 def test_lev_mar_executable_exists():
     rt_model = RTModel()
     lev_mar_executable_path = Path(rt_model.bindir + rt_model.levmarexe)
     assert lev_mar_executable_path.exists()
 
+
 def test_model_selector_executable_exists():
     rt_model = RTModel()
     model_selector_executable_path = Path(rt_model.bindir + rt_model.modelselectorexe)
     assert model_selector_executable_path.exists()
+
 
 def test_finalizer_executable_exists():
     rt_model = RTModel()
