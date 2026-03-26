024d049269e6b5a044003ad97d2e6ab751761ce9
Antonio Consiglio <aconsiglio@unisa.it>
2026-03-26 14:20:57 +0100

    Add files via upload

--- RTModel/include/LevMarFit.h
+++ RTModel/include/LevMarFit.h
@@ -38,14 +38,15 @@ class LevMar {
 	double* pr, * prn, * sumy, * sumy2, * sumsigma, * sumfy, * sumf, * sumf2, * limbdarks;
 	double* sumsigmaN, * sumsigmaE, * sumcN, * sumcE, * sumc1, * sumc2;
 	int* sizes, * starts;
-	double inc;
-	double* GradVec;
-	double normgrad;
 
 	int consnumber, * consindex;
 	double* constraints, * consleft, * consright, * consvars;
 	int modnumber;
 
+	double tmaxmax;
+	double maxmaxsum;
+	double y1maxmax;
+	double y2maxmax;
 	double Tol;
 
 	//	void (LevMar::* PrintOut)(double*);
@@ -76,6 +77,5 @@ public:
 
 double Determinant(double*, int);
 void Inverse(double*, double*, int);
-
 
 #endif
